6092fe365d435be4a95c85de583b648624fcfcc5
blujagu <lawebtek@gmail.com>
2016-02-18 16:45:44 -0700

    Passing Test: Acquire access token

--- test/unit-tests-ui.js
+++ test/unit-tests-ui.js
@@ -1,10 +1,20 @@
-define(['testharness', 'request', 'db'],
-    function (harness, request, db) {
+define(['testharness', 'request'],
+    function (harness, request) {
         return {
             run: function () {
                 "use strict";
+
+                harness.async_test(function (test) {
+                    request('http://canvas-api.herokuapp.com/api/v1/tokens', 'post').then(function (xhr) {
+                        var responseText = xhr.responseText;
+
+                        test.step(function () {
+                            harness.assert_true(responseText && JSON.parse(responseText) && xhr.statusText === "Created");
+                            test.done();
+                        });
+                    });
+                }, "Acquire access token from Canvas API");
                 
-                console.log('Test harness loaded successfully.')
             }
         };
     }
