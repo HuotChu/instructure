683b4b04b8ed1a6d0268e97ea186369584ef1ce5
blujagu <lawebtek@gmail.com>
2016-02-18 16:11:22 -0700

    Initial Commit: Boilerplate framework

---
+++ test/unit-tests-ui.js
@@ -0,0 +1,11 @@
+define(['testharness', 'request', 'db'],
+    function (harness, request, db) {
+        return {
+            run: function () {
+                "use strict";
+                
+                console.log('Test harness loaded successfully.')
+            }
+        };
+    }
+);
