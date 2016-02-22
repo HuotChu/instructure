1b44545a0c4e61c4b20b2dffc70cebe837e9e6b6
blujagu <lawebtek@gmail.com>
2016-02-21 19:42:54 -0700

    Added event handlers

--- app/controller.js
+++ app/controller.js
@@ -1,44 +1,55 @@
 define(['box', 'model'], function(Box, modelPromise) {
     'use strict';
     
-    var formatDate = function (dateStr) {
-            var date = new Date(dateStr).toString();
+    var containerDOM = document.querySelector('#dynamic-content'),
+        formatDate = function (dateStr) {
+        var date = new Date(dateStr).toString();
 
-            date = date.replace(/\w{3,4}\s(\w{3})\s(\d{1,2}).*/, function (m, p1, p2) {
-                var regX = new RegExp(p1, 'i'),
-                    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-                    fullName = '';
-                
-                months.some(function (month) {
-                    if (regX.test(month)) {
-                        fullName = month;
-                        return true;
-                    }
-                });
-                
-                return fullName + ' ' + p2 + ', 2016';
-            });
-        
-            return date;
-        }, 
-        transformer = function (data) {
-            var i = 0,
-                returnArray = [],
-                course;
+        date = date.replace(/\w{3,4}\s(\w{3})\s(\d{1,2}).*/, function (m, p1, p2) {
+            var regX = new RegExp(p1, 'i'),
+                months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                fullName = '';
             
-            for (i; i < 2; ++i) {
-                course = data[i];
-                returnArray[i] = {
-                    position: i + 1,
-                    id: course.id,
-                    courseName: course.name,
-                    description: course.description,
-                    startDate: formatDate(course.start_at)
+            months.some(function (month) {
+                if (regX.test(month)) {
+                    fullName = month;
+                    return true;
                 }
-            }
+            });
             
-            return returnArray;
-        };
+            return fullName + ' ' + p2 + ', 2016';
+        });
+    
+        return date;
+    }, 
+    transformer = function (data) {
+        var i = 0,
+            returnArray = [],
+            date,
+            course;
+        
+        for (i; i < 2; ++i) {
+            course = data[i];
+            date = course.start_at ? course.start_at.split(/\s/) : 'TBA';
+            date = date instanceof Array ? formatDate(date[0]) : date;
+            returnArray[i] = {
+                position: i + 1,
+                id: course.id,
+                courseName: course.name,
+                description: course.description,
+                startDate: date
+            }
+        }
+        
+        return returnArray;
+    },
+    doEnroll = {
+        event: 'click',
+        id: '_enrollButton_',
+        callback: function (id) {
+            console.log('Enroll button ' + id + ' clicked.');
+        }
+    };
 
     modelPromise.then(function (model) {
         var courseData = model.select('*').from('Courses').go(),
@@ -49,16 +60,49 @@ define(['box', 'model'], function(Box, modelPromise) {
                     courses: transformer(courseData)
                 },
     
-                target: document.querySelector('#dynamic-content'),
+                target: containerDOM,
     
                 template: 'app/course-view.html',
     
-                domEvents: [],
+                domEvents: [
+                    doEnroll,
+                    {
+                        event: 'click',
+                        id: 'backButton',
+                        callback: function () {
+                            // previous page
+                            model.getPage(-1);
+                        }
+                    },
+                    {
+                        event: 'click',
+                        id: 'nextButton',
+                        callback: function () {
+                            // next page
+                            model.getPage(1);
+                        }
+                    }
+                ],
     
-                modelEvents: []
+                modelEvents: [
+                    {
+                        event: 'Canvas.State.current_page.update',
+                        id: 'page',
+                        callback: function (e, el, box) {
+                            var pageNumber = e.detail.value;
+
+                            containerDOM.innerHTML = '';
+                            config.target = containerDOM;
+                            config.data = {
+                                courses: transformer(model.select('*').from('Courses').where('page', '===', pageNumber).go())
+                            };
+                            config.modelEvents = [];
+
+                            courseView = new Box(config);
+                        }
+                    }            
+                ]
             },
-            courseView = new Box(config).view;
-        
-        
+            courseView = new Box(config);
     });
 });
